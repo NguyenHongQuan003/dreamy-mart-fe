0f05cb7e37f3af572dcadd7656fdde670bbe4f95
NguyenHongQuan <nhquan2622003@gmail.com>
2025-03-18 23:35:52 +0700

    fix loi truyen tham so function gitCurrentUser

--- src/services/authService.js
+++ src/services/authService.js
@@ -14,14 +14,27 @@ export const login = async (email, password) => {
   throw new Error("Invalid credentials");
 };
 
+// Có thể xảy ra lỗi không thể lấy thông tin user do cách dùng path param hoặc query params
+// Khắc phục tạm thời bằng cách bắt lỗi
+// nếu lỗi khi dùng path params thì dùng query params
+// path param: API_URL/users/id (dùng khi id là string)
+// query param: API_URL/users?id=id (dùng khi id là number)
 export const getCurrentUser = async (token) => {
   const response = await axios.get(`${API_URL}/tokens?token=${token}`);
   if (response.data.length > 0) {
     const tokenData = response.data[0];
-    const userResponse = await axios.get(
-      `${API_URL}/users/${tokenData.userId}`
-    );
-    return userResponse.data;
+    try {
+      const userResponse = await axios.get(
+        `${API_URL}/users/${tokenData.userId}` // cách dùng path params
+      );
+      return userResponse.data;
+    } catch (pathError) {
+      pathError;
+      const userResponse = await axios.get(
+        `${API_URL}/users?id=${tokenData.userId}` // cách dùng query params
+      );
+      return userResponse.data[0];
+    }
   }
   throw new Error("Invalid token");
 };
